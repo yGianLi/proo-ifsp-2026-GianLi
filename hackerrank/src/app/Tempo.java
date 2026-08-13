3ccb5a3708b947b3a04f70ebb40ff5a0e64b3eb8
GIAN LI ZHANG <bv3071251@ad.sbv.ifsp.edu.br>
2026-08-13 09:36:30 -0300

    Adicionamos o código da classe tempo para calcular o tempo de jogo

---
+++ hackerrank/src/app/Tempo.java
@@ -0,0 +1,23 @@
+package app;
+import java.util.Scanner;
+public class Tempo {
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Digite o valor da Hora inicial: ");
+        int Hi = scan.nextInt();
+        System.out.print("Digite o valor do Minuto Inicial: ");
+        int Mi = scan.nextInt();
+        System.out.print("Digite o valor da Hora final: ");
+        int Hf = scan.nextInt();
+        System.out.print("Digite o valor do Minuto Final: ");
+        int Mf = scan.nextInt();
+
+        int HorasT = Hf - Hi;
+        int MinutosT = Mf- Mi;
+
+        System.out.print("O jogo durou " + HorasT + " horas e " + MinutosT + " minutos");
+
+
+    }
+
+}
