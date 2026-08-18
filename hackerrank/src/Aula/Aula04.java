6580e3c458af84bc254e7f90efa245a2323c3043
GianLi <gianlizhang@gmail.com>
2026-08-18 11:03:53 -0300

    anotações da aula

---
+++ hackerrank/src/Aula/Aula04.java
@@ -0,0 +1,35 @@
+package Aula;
+
+public class Aula04 {
+    // Obs:
+    // System.exit(0); - Fecha o Programa
+
+
+
+    // Estrutura de Seleção;
+    // "switch-case" e "if else"
+
+
+
+
+    // Estrutura de Repetição;
+    // "for", "while" e "do while"
+    // Diferença entre eles:
+
+    // for repetição finita, while repetir até que a condição seja inválida, do while condição já começa com 1 repetição e dps repete inumeras vezes até a condição ser falsa
+    // for = numero de repetições é conhecido
+    // while = numero de repetições é desconhecida
+    // do while = já inicia com 1 repetição
+
+
+    // for (início; condição(parada); passo(incremento));
+
+    // while (condição(parada));
+    // while(i <= 5){
+
+
+    // i++; condição para não deixar o while ficar em um looping infinito;
+
+    // do while();
+
+}
