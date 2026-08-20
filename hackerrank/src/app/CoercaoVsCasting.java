e879a61e46033bf932afa68d7de4f8473a96fbc2
GIAN LI ZHANG <bv3071251@ad.sbv.ifsp.edu.br>
2026-08-20 10:14:45 -0300

    Exercicio CoercaoVSCasting

--- hackerrank/src/app/CoercaoVsCasting.java
+++ hackerrank/src/app/CoercaoVsCasting.java
@@ -1,4 +1,33 @@
 package app;
 
 public class CoercaoVsCasting {
+    public static void main(String[] args){
+        long populacaoCidade = 1_500_000; // (1)
+
+        int votosApurados = 1_499_499;
+        long totalGeral = votosApurados; // (2)
+
+        float mediaNotas = 8; // (3)
+        double notaFinal = mediaNotas; // (4)
+
+        double saldoConta = 2500.75;
+        long saldoEmCentavosAproximado = (long) (saldoConta * 100); // (5)
+
+        int quantidadeItens = 10;
+        float precoUnitario = 3.5f;
+        float totalCompra = quantidadeItens * precoUnitario; // (6)
+
+        double mediaFinal = 7.8;
+        int mediaParaAprovacao = (int) mediaFinal; // (7)
+
+        System.out.println("populacaoCidade = " + populacaoCidade);
+        System.out.println("totalGeral = " + totalGeral);
+        System.out.println("notaFinal = " + notaFinal);
+        System.out.println("saldoEmCentavosAproximado = " + saldoEmCentavosAproximado);
+        System.out.println("totalCompra = " + totalCompra);
+        System.out.println("mediaParaAprovacao = " + mediaParaAprovacao);
+
+    }
+
+
 }
