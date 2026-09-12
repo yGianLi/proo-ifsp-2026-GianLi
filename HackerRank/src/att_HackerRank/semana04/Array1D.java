ace3bddb07a01cd9a107de62ffd17dd2d6d4b0a1
GianLi <gianlizhang@gmail.com>
2026-09-11 21:39:02 -0300

    resolvido o exercicio 1d array

--- HackerRank/src/att_HackerRank/semana04/Array1D.java
+++ HackerRank/src/att_HackerRank/semana04/Array1D.java
@@ -1,7 +1,36 @@
 package att_HackerRank.semana04;
 
-public class Array1D {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
+public class Array1D {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int[] myArray = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            myArray[i] = Integer.parseInt(bufferedReader.readLine().trim());
+        }
+
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println(myArray[i]);
+        }
+
+
+        bufferedReader.close();
+    }
 
 
 }
--- HackerRank/src/att_HackerRank/semana04/Array2D.java
+++ HackerRank/src/att_HackerRank/semana04/Array2D.java
@@ -1,4 +1,37 @@
 package att_HackerRank.semana04;
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
 public class Array2D {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, 6).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+    }
+
 }
